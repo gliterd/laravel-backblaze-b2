04600155a4f68fdded29e9164a5bbe112d423b79
Ramesh Mhetre <mhetreramesh@gmail.com>
2017-12-19 12:21:41 +0530

    Test case part updated
    Signed-off-by: Ramesh Mhetre <mhetreramesh@gmail.com>

--- tests/BackblazeB2ServiceProviderTests.php
+++ tests/BackblazeB2ServiceProviderTests.php
@@ -1,5 +1,7 @@
 <?php
+namespace Gliterd\BackblazeB2\Tests;
 
 class BackblazeB2ServiceProviderTests extends PHPUnit_Framework_TestCase
 {
+    //TODO: Find a correct way to write unit test case for service provider
 }
